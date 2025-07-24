5671fbdf59f985d0a6becf19bdb5ab775d844b25
Lada Koicheva <koichevalada@gmail.com>
2025-07-24 13:32:28 +0300

    Initial commit:scraffolded React app with basic structure

---
+++ todoapp/src/App.js
@@ -0,0 +1,40 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todos</h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label> Title </label>
+            <input type="text" placeholder="What's the task title " />
+
+            <div className="todo-input-item">
+              <label> Description </label>
+              <input type="text" placeholder="What's the task description " />
+            </div>
+
+            <div className="todo-input-item">
+              <button type="button" className=" Primary button">
+                Add
+              </button>
+            </div>
+          </div>
+          <div className="btn-area"> 
+            <button> Todo</button>
+            <button>Completed</button>
+          </div>
+          <div className="todo-list"> 
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
