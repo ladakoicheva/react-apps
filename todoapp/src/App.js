587bfdec223c52afeee7ecb5ff3e8ce6cb6bd43f
Lada Koicheva <koichevalada@gmail.com>
2025-07-24 18:03:05 +0300

    basic styling applied except task1 and description section

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,7 +1,9 @@
-
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+
   return (
     <div className="App">
       <h1>My Todos</h1>
@@ -10,28 +12,39 @@ function App() {
           <div className="todo-input-item">
             <label> Title </label>
             <input type="text" placeholder="What's the task title " />
-
-            <div className="todo-input-item">
-              <label> Description </label>
-              <input type="text" placeholder="What's the task description " />
-            </div>
-
-            <div className="todo-input-item">
-              <button type="button" className=" Primary button">
-                Add
-              </button>
-            </div>
           </div>
-          <div className="btn-area"> 
-            <button> Todo</button>
-            <button>Completed</button>
+          <div className="todo-input-item">
+            <label> Description </label>
+            <input type="text" placeholder="What's the task description " />
           </div>
-          <div className="todo-list"> 
-            <h3>Task 1</h3>
-            <p>Description</p>
+          <div className="todo-input-item">
+            <button type="button" className="PrimaryBtn">
+              Add
+            </button>
           </div>
         </div>
-
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn ${
+              isCompleteScreen === false && "active"
+            }`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
+            {" "}
+            Todo
+          </button>
+          <button
+            className={`secondaryBtn ${
+              isCompleteScreen === true && "active"
+            }`}
+            onClick={() => setIsCompleteScreen(true)}>
+            Completed
+          </button>
+        </div>
+        <div className="todo-list">
+          <h3>Task 1</h3>
+          <p>Description</p>
+        </div>
       </div>
     </div>
   );
